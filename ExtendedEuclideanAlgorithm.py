a39bd307d2aac4eedc872497ea6067bcc58ad89a
zsidoruben <zsruben03@gmail.com>
2023-11-02 22:51:04 +0100

    gdcExtended

--- ExtendedEuclideanAlgorithm.py
+++ ExtendedEuclideanAlgorithm.py
@@ -1,43 +1,21 @@
-import math
-from tkinter import S
-from typing import Self
 
-def gcd(a, b):
-    print("(",a,",",b,")")
-    #switch them if b is bigger
-    if b > a:
-        c = a
-        a = b
-        b = c
-    remains = a % b
-    #if the remains is 0, than the last remains is the gdc
-    if remains == 0:
-        print(b)
-        return
-    #if what remains is not 0, repeat the algorithm, by giving it the smaller number(b) and the remains
-    gcd(b, remains)
-    
 
-gcd(54, 888)
+def gcdExtended(a, b): 
+    # Base Case 
+    if a == 0 : 
+        return b,0,1
+             
+    gcd,x1,y1 = gcdExtended(b%a, a) 
+     
+    # Update x and y using results of recursive 
+    # call 
+    x = y1 - (b//a) * x1 
+    y = x1 
+     
+    return gcd,x,y 
 
-#save each equasion
-#create a substitutuon for the remains
-#from the second to last equasion, go backwards and use the substitutions for the numbers
-
-class Eq:
-    def __init__(self, nagyobb, kisebb, megvan, maradek):
-        self.nagyobb = nagyobb
-        self.kisebb = kisebb
-        self.megvan = megvan
-        self.maradek = maradek
-
-    """
-    nagyobb = oszto * megvanennyiszer + maradek
-
-    nagyobb - oszto * megvanennyiszer = maradek
-
-    substitution: nagyobb + oszto(substituted) * -megvan
-    
-    """
-
-    pass
+# Driver code 
+a, b = 35,15
+gcd, x, y = gcdExtended(a, b) 
+print("gcd(", a , "," , b, ") = ", gcd)
+print(gcd, "=", a, "*", x, "+", b, "*", y)
